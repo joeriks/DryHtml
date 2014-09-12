6708fc30270164906f63c13e47bead45b720f1aa
Jonas Eriksson <jonas@programenta.se>
2014-09-12 13:35:44 +0200

    moved resource to a helper

---
+++ DryHtml.Tests/Helpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DryHtml.Tests
+{
+    public static class Helpers
+    {
+        public static string GetResource(string resourceNameSpace)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using (Stream stream = assembly.GetManifestResourceStream(resourceNameSpace))
+            using (StreamReader reader = new StreamReader(stream))
+                return reader.ReadToEnd();
+        }
+    }
+}
--- DryHtml.Tests/Readme.cs
+++ DryHtml.Tests/Readme.cs
@@ -11,15 +11,9 @@ namespace DryHtml.Tests
 {
     public class Readme : DryHtmlDocument
     {
-        private static string GetResource(string resourceNameSpace)
-        {
-            var assembly = Assembly.GetExecutingAssembly();
-            using (Stream stream = assembly.GetManifestResourceStream(resourceNameSpace))
-                using (StreamReader reader = new StreamReader(stream))
-                    return reader.ReadToEnd();
-        }
+        
         public Readme(DocumentViewModel model)
-            : base(GetResource("DryHtml.Tests.Readme.html"))
+            : base(Helpers.GetResource("DryHtml.Tests.Readme.html"))
         {
             this.SetHtml("//title", model.Title);
             this.SetHtml("{header}/h1", model.Header);
