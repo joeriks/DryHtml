cb7ae1cc10aa512cd871cf958b4bbdaab0e2be0c
joeriks <jonas@programenta.se>
2014-09-14 23:14:32 +0200

    Fixed nth child

--- DryHtml.HtmlDiff.Tests/BasicDiffs.cs
+++ DryHtml.HtmlDiff.Tests/BasicDiffs.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CsQuery;
 
 namespace DryHtml.HtmlDiff.Tests
 {
@@ -9,20 +10,36 @@ namespace DryHtml.HtmlDiff.Tests
         [TestMethod]
         public void FindDiff()
         {
-            var x = new HtmlDiff.Comparer("<html><body><p>A</p></html>", "<html><p>B</p></body></html>");
+            var compareHtml = "<html><body><p>A</p></html>";
+            var x = new HtmlDiff.Comparer(compareHtml, "<html><p>B</p></body></html>");
             Assert.AreEqual("A", x.Diffs[0].compareSourceHtml);
             Assert.AreEqual(true, x.Diffs[0].isText);
             Assert.AreEqual("html > body > p", x.Diffs[0].selector);
+
             Assert.AreEqual(1, x.Diffs.Count);
+
+            var useComputedSelector = ((CQ)compareHtml)[x.Diffs[0].selector].Text();
+            Assert.AreEqual("A", useComputedSelector);
+
+            var useWrittenSelector = ((CQ)compareHtml)["html > body > p"].Text();
+            Assert.AreEqual("A", useWrittenSelector);
+
+
         }
         [TestMethod]
         public void FindNthChild()
         {
-            var x = new HtmlDiff.Comparer("<html><body><p>A</p><p>A</p></html>", "<html><p>A</p><p>B</p></body></html>");
-            Assert.AreEqual("A", x.Diffs[0].compareSourceHtml);
+            var compareHtml = "<html><body><p>A</p><p>C</p></html>";
+            var x = new HtmlDiff.Comparer(compareHtml, "<html><p>A</p><p>B</p></body></html>");
+            Assert.AreEqual("C", x.Diffs[0].compareSourceHtml);
             Assert.AreEqual(true, x.Diffs[0].isText);
-            Assert.AreEqual("html > body > p:nth-child(1)", x.Diffs[0].selector);
+            Assert.AreEqual("html > body > p:nth-child(2)", x.Diffs[0].selector);
             Assert.AreEqual(1, x.Diffs.Count);
+
+            var useComputedSelector = ((CQ)compareHtml)["html > body > p:nth-child(2)"].Text();
+            Assert.AreEqual("C", useComputedSelector);
+
+
         }
     }
 }
---
+++ DryHtml.HtmlDiff/Comparer.cs
@@ -0,0 +1,165 @@
+﻿using CsQuery;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DryHtml.HtmlDiff
+{
+    public class Comparer
+    {
+        private string compareHtml;
+        private string compareToHtml;
+        private string selector;
+
+        public Comparer(string compareHtml, string compareToHtml, string selector = "*")
+        {
+            this.compareHtml = compareHtml;
+            this.compareToHtml = compareToHtml;
+            this.selector = selector;
+            compare();
+        }
+
+        private bool compare()
+        {
+            if (selector == "") selector = "*";
+            var compareHtmlCq = ((CQ)compareHtml).Select(selector).Get(0);
+            var compareToHtmlCq = ((CQ)compareToHtml).Select(selector).Get(0);
+            return TraverseNode(compareHtmlCq, compareToHtmlCq, "");
+        }
+
+        public List<Diff> Diffs = new List<Diff>();
+        public struct Diff
+        {
+            public string selector;
+            public bool isText;
+            public string compareSourceHtml;
+            public string compareWithHtml;
+        }
+
+        bool CompareNodes(IDomObject n, IDomObject compareToNode, string path)
+        {
+
+            var outerHtml = n.Render();
+            var compareToOuterHtml = (compareToNode == null) ? "" : compareToNode.Render();
+
+            var getAttributes = new Func<IDomObject, string>(f => (f.Attributes == null) ? "" : string.Join(",", f.Attributes.Select(a => a.Key + "=" + a.Value)));
+
+            var thisNodeEquals = (compareToNode != null
+                && n.NodeName == compareToNode.NodeName
+                && n.NodeType == compareToNode.NodeType
+                && n.NodeValue == compareToNode.NodeValue)
+                && getAttributes(n) == getAttributes(compareToNode);
+
+
+            var nodeHtmlEquals = (compareToNode != null && outerHtml == compareToOuterHtml);
+
+            var pathAttributes = "";
+            if (n.Attributes != null)
+            {
+                var nSelectorAttributes = n.Attributes.Where(a => a.Key == "id" || a.Key == "class");
+                if (nSelectorAttributes.Any())
+                {
+                    var idAttribute = nSelectorAttributes.Where(sa => sa.Key == "id");
+                    var classAttribute = nSelectorAttributes.Where(sa => sa.Key == "class");
+
+                    if (idAttribute.Any()) pathAttributes = "#" + idAttribute.First().Value;
+                    else
+                        if (classAttribute.Any()) pathAttributes = "." + classAttribute.First().Value;
+
+                }
+            }
+            path = path + ">" + n.NodeName + pathAttributes + "(" + n.Index.ToString() + ")";
+
+            if (thisNodeEquals)
+            {
+                if (n.HasChildren && compareToNode != null)
+                {
+                    return TraverseNode(n, compareToNode, path);
+                }
+                return true;
+
+            }
+            else
+            {
+
+                var p = n;
+                var pp = "";
+                var breakNow = false;
+
+                var diff = new Diff();
+
+                do
+                {
+
+                    if (p != null && p.Attributes != null)
+                    {
+                        var idAttr = p.Attributes.Where(a => a.Key == "id");
+                        if (idAttr.Any())
+                        {
+                            pp = p.NodeName + "#" + idAttr.First().Value + " " + pp;
+                            breakNow = true;
+                        }
+                    }
+                    var hasClassAttribute = false;
+                    if (!breakNow)
+                    {
+                        if (p != null && p.Attributes != null)
+                        {
+                            var idCssClass = p.Attributes.Where(a => a.Key == "class");
+                            if (idCssClass.Any())
+                            {
+                                hasClassAttribute = true;
+                                pp = p.NodeName.ToLower() + "." + idCssClass.First().Value.Replace(" ", ".") + ((pp!="")?" > " + pp:"");
+                            }
+                        }
+                    }
+                    if (!breakNow && !hasClassAttribute)
+                    {
+                        if (p.NodeType == NodeType.TEXT_NODE) 
+                            diff.isText = true;
+                        if (p.NodeType == NodeType.ELEMENT_NODE)
+                        {
+                            var indexSelector = "";
+                            if (p.ParentNode.ChildNodes.Count(t=>t.NodeName==p.NodeName)> 1) indexSelector = ":nth-child(" + (p.Index+1).ToString() + ")";
+                            pp = p.NodeName.ToLower() + indexSelector + ((pp != "") ? " > " + pp : ""); //(" + p.Index.ToString() + ")
+                        }
+                    }
+
+                    p = p.ParentNode;
+                }
+                while (!(p == null || breakNow));
+
+                diff.selector = pp;
+                diff.compareSourceHtml = outerHtml;
+                diff.compareWithHtml = compareToOuterHtml;
+                Diffs.Add(diff);
+
+                return false;
+
+            }
+
+        }
+
+        bool TraverseNode(IDomObject node, IDomObject compareToNode, string path)
+        {
+            var result = true;
+            if (node != null && node.HasChildren)
+                foreach (IDomObject n in node.ChildNodes)
+                {
+
+                    var compareToMissing = (compareToNode == null || !compareToNode.HasChildren || n.Index >= compareToNode.ChildNodes.Length);
+
+                    IDomObject compareToNodeChild = null;
+                    if (!compareToMissing) compareToNodeChild = compareToNode.ChildNodes[n.Index];
+                    var childResult = CompareNodes(n, compareToNodeChild, path);
+                    if (!childResult) result = false;
+                }
+            return result;
+        }
+
+
+    }
+
+}
