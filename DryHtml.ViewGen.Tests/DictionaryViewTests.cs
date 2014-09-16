5e40b88cbb0fff317c06dd492be2f2c7b4db3fd5
joeriks <jonas@programenta.se>
2014-09-16 08:17:29 +0200

    fsw

---
+++ DryHtml.ViewGen.Tests/DictionaryViewTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace DryHtml.Tests
+{
+    [TestClass]
+    public class DictionaryViewTests
+    {
+
+
+        [TestMethod]
+        [DeploymentItem("SamplePrototype.html")]
+        [DeploymentItem("SampleGeneratedView.cshtml")]
+        public void TestGenerationFromDictionary()
+        {
+
+
+            // Arrange
+
+            var prototypeHtml = System.IO.File.ReadAllText("SamplePrototype.html");
+            var expectedResult = System.IO.File.ReadAllText("SampleGeneratedView.cshtml");
+
+            var modelDictionary = new Dictionary<string, string>();
+            modelDictionary.Add("Header", "h1");
+            modelDictionary.Add("Description", "p");
+
+            // Act
+
+            var generatedResult = new ViewGen.ViewGenerator(prototypeHtml, "#header", modelDictionary).CsHtmlView;
+
+            // Assert
+
+            Assert.AreEqual(expectedResult, generatedResult);
+
+
+        }
+
+
+    }
+}
--- DryHtml.ViewGenConsole/Program.cs
+++ DryHtml.ViewGenConsole/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 using System;
+using System.IO;
+using System.Linq;
 namespace DryHtml.ViewGenConsole
 {
     class Program
@@ -9,14 +11,33 @@ namespace DryHtml.ViewGenConsole
 
             var filename = (args.Length == 0) ? "viewgen.json" : args[0];
 
+            generate(filename);
+
+            if (args.Any(t => t == "-watch" || t == "-w"))
+            {
+                var f = new FileSystemWatcher(Environment.CurrentDirectory, "*.*");
+                f.Changed += f_Changed;
+                f.EnableRaisingEvents = true;
+                Console.WriteLine("Watching " + Environment.CurrentDirectory + " for changes");
+                Console.ReadLine();
+            }
+
+
+
+        }
+
+        static void generate(string filename)
+        {
             var viewGen = new DryHtml.ViewGen.ViewGenerator(filename);
-
             viewGen.OutputRootPath = Environment.CurrentDirectory;
-
             viewGen.GenerateAll();
+            Console.WriteLine("Generated files");
 
-            Console.WriteLine("Done");
+        }
 
+        static void f_Changed(object sender, FileSystemEventArgs e)
+        {
+            generate(e.FullPath);
         }
     }
 }
